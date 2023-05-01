384213ee868cba0cbe16cf41ae612e0952553d70
Eduardo da Silva <eduardo.silva@gmail.com>
2023-05-01 15:56:51 -0300

    add react-navigatio/native-stack with Login screen

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
